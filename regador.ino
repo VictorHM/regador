d26e87148c400cdc239d5d7ee76db33e5687d1f0
Victor Hevia Martin <rcpoetics@gmail.com>
2023-07-29 22:04:48 +0200

    Measures each 5s. Detects button during elapsed.

--- regador.ino
+++ regador.ino
@@ -26,11 +26,16 @@ int soil_value;
 ********************************/
 bool isButtonPressed = false;
 int buttonPin = 3;
+int button; // To keep track of button pressed to know when we put water.
+
+unsigned long elapsedTime;
+unsigned long startCount;
 
 void setup() {
   // Inicializamos comunicación serie
   Serial.begin(9600);
- 
+  elapsedTime = 0;
+  startCount = millis();
   // Comenzamos el sensor DHT
   dht.begin();
   // initialize the pushbutton pin as an input:
@@ -40,64 +45,72 @@ void setup() {
 
 void loop() {
     // Esperamos 5 segundos entre medidas
-  delay(5000);
- 
-  // Leemos la humedad relativa
-  float h = dht.readHumidity();
-  // Leemos la temperatura en grados centígrados (por defecto)
-  float t = dht.readTemperature();
+  unsigned long currentTime = millis();
+  elapsedTime = currentTime - startCount;
+
   // Comprobamos si hay pulsacion de boton, que indica riego.
-  int button = digitalRead(buttonPin);
-  if (button != LOW) {
+  button = digitalRead(buttonPin);
+  if (button != LOW && !isButtonPressed) {
     isButtonPressed = true;
   }
+
+  if (elapsedTime > 5000) {
+    elapsedTime = 0;
+    startCount = millis();
+    // Leemos la humedad relativa
+    float h = dht.readHumidity();
+    // Leemos la temperatura en grados centígrados (por defecto)
+    float t = dht.readTemperature();
+
  
-  // Comprobamos si ha habido algún error en la lectura
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Error obteniendo los datos del sensor DHT11");
-    return;
-  }
+    // Comprobamos si ha habido algún error en la lectura
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Error obteniendo los datos del sensor DHT11");
+      return;
+    }
 
-  // Calcular el índice de calor en grados centígrados
-  float hic = dht.computeHeatIndex(t, h, false);
+    // Calcular el índice de calor en grados centígrados
+    float hic = dht.computeHeatIndex(t, h, false);
 
-  // Leemos soil moisture
-  soil_value = readSensor(soil_pin);
-  // Map to percentage
-  //soil_value = map(soil_value, 550, 0, 0, 100);
-  // Print to serial console.
-  Serial.print("Humedad");
-  Serial.print("\t");
-  Serial.print("Temperatura");
-  Serial.print("\t");
-  Serial.print("Indice calor");
-  Serial.print("\t");
-  Serial.print("Humedad Suelo");
-  Serial.print("\t");
-  Serial.print("Is Button Pressed? ");
-  Serial.print("\n");
-  Serial.print(h);
-  Serial.print("\t");
-  Serial.print("\t");
-  Serial.print(t);
-  Serial.print("\t");
-  Serial.print("\t");
-  Serial.print(hic);
-  Serial.print("\t");
-  Serial.print("\t");
-  Serial.print(soil_value);
-  Serial.print("\t");
-  Serial.print("\t");
-  if (isButtonPressed) {
-    Serial.print("True");
-  } else {
-    Serial.print("False");
-  }
+    // Leemos soil moisture
+    soil_value = readSensor(soil_pin);
+    // Map to percentage
+    //soil_value = map(soil_value, 550, 0, 0, 100);
+    // Print to serial console.
+    Serial.print("Humedad");
+    Serial.print("\t");
+    Serial.print("Temperatura");
+    Serial.print("\t");
+    Serial.print("Indice calor");
+    Serial.print("\t");
+    Serial.print("Humedad Suelo");
+    Serial.print("\t");
+    Serial.print("Is Button Pressed? ");
+    Serial.print("\n");
+    Serial.print(h);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(t);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(hic);
+    Serial.print("\t");
+    Serial.print("\t");
+    Serial.print(soil_value);
+    Serial.print("\t");
+    Serial.print("\t");
+    if (isButtonPressed) {
+      Serial.print("True");
+    } else {
+      Serial.print("False");
+    }
+
+    Serial.print("\n");
   
-  Serial.print("\n");
- 
-  // Reset the value of the button
-  isButtonPressed = false;
+    // Reset the value of the button
+    isButtonPressed = false;
+    delay(1000);
+  }
 }
 
 //  This function returns the analog soil moisture measurement
